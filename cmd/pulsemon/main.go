ff1879e117b2f7fc3fb454786be281c49e79f104
Ganeshspace12 <mhetreg458@gmail.com>
2026-09-25 11:37:49 +0530

    feat: add periodic health checks

--- cmd/pulsemon/main.go
+++ cmd/pulsemon/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/Ganesh-12-spec/pulsemon/internal/checker"
 )
 
 func main() {
-	err := checker.Check("https://example.com")
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
 
-	if err != nil {
-		fmt.Println("Health check failed:", err)
-		return
+	fmt.Println("Pulsemon started...")
+
+	for range ticker.C {
+		err := checker.Check("https://example.com")
+
+		if err != nil {
+			fmt.Println("Health check failed:", err)
+			continue
+		}
+
+		fmt.Println("Health check passed")
 	}
-
-	fmt.Println("Health check passed")
 }
